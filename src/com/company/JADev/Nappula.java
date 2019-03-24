bcf1a009c96550521ff215165b61dd21c3ff0f6c
Aleksi <onni.inkiva@gmaill.com>
2019-03-24 17:32:21 +0200

    Nappulaa ja Sotilasta tehty

--- src/com/company/JADev/Nappula.java
+++ src/com/company/JADev/Nappula.java
@@ -2,5 +2,39 @@ package com.company.JADev;
 
 public abstract class Nappula {
 
+    boolean onValkoinen;
+
+    /**
+     * Jos nappula on valkoinen, aseta true, jos musta, aseta false
+     * @param onValkoinen
+     */
+    public Nappula(boolean onValkoinen) {
+        this.onValkoinen = onValkoinen;
+    }
+
+    /**
+     * @return true, jos valkoinen, muuten false
+     */
+    public boolean onValkoinen() {
+        return onValkoinen;
+    }
+
+    /**
+     * Piirtää annetun nappulan konsoliin
+     */
+    public abstract void piirra();
+
+    /**
+     * Tarkistaa onko siirto sallitu
+     * @return true, jos siirto on sallittu, false ei sallittu siirto
+     */
+    public abstract boolean onkoSallittu(int aX, int aY, int bX, int bY);
+
+    /**
+     * @return
+     */
+    public abstract int arvo();
+
+
 
 }
--- src/com/company/JADev/Sotilas.java
+++ src/com/company/JADev/Sotilas.java
@@ -1,4 +1,45 @@
 package com.company.JADev;
 
 public class Sotilas extends Nappula {
+
+
+    public Sotilas(boolean onValkoinen) {
+        super(onkoValkoinen);
+    }
+
+    @Override
+    public void piirra() {
+        if(this.onValkoinen) {
+            System.out.print("\u2659");
+        }
+        if(!(this.onValkoinen)) {
+            System.out.print("\u265F");
+        }
+    }
+
+    @Override
+    public boolean onkoSallittu(int aX, int aY, int bX, int bY) {
+
+        /**
+         * Saa liikkua 1 eteenpäin tai aloitusruudusta
+         * 2 eteenpäin tai syödä vastapuolen nappulan
+         */
+        if (this.onValkoinen) {
+            return (((aY == bY) && aX == (bX + 1))
+                    || ((aX == 6) (aY == bY) && (aX == (bX + 2)))
+                    ||((aX == (bX + 1)) && (Math.abs(aY - bY) == 1)));
+        }
+        else {
+            return (((aY == bY) && aX == (bX -1))
+                    || ((aX == 1) && (aY == bY) &&(aX == (bX - 2)))
+                    || ((aX == (bX - 1)) && (Math.abs(aY - bY) == 1)));
+        }
+
+    }
+
+    @Override
+    public int arvo() {
+        return 1;
+    }
+
 }
