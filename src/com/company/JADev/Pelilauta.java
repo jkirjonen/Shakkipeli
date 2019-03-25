532fc53e96d685e292cd8f683c1197ecd3c09cf8
jkirjonen <kirjonen@gmail.com>
2019-03-25 20:18:41 +0200

    Pelilauta luokka lisätty

---
+++ src/com/company/JADev/Pelilauta.java
@@ -0,0 +1,20 @@
+package com.company.JADev;
+
+public class Pelilauta {
+    private Nappula[][] pelilauta = New Nappula[8][8];
+
+
+    public Pelilauta(){
+
+    }
+
+    private static void asetaPelilauta(){
+
+
+        for(int rivi=0;rivi<pelilauta.length;rivi++){
+            for(int pysty=0;pysty<pelilauta[0].length;pysty++){
+                if(rivi == 0 && pysty == 0)
+            }
+        }
+    }
+}
